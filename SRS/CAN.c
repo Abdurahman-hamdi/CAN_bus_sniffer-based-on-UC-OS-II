37ff1ee91233c32485d6b97ac816988a12aa8bc4
Abdurahman hamdi <abdurrahman.hamdi.95@gmail.com>
2024-08-12 04:42:27 +0300

    Update CAN.c

--- SRS/CAN.c
+++ SRS/CAN.c
@@ -29,20 +29,19 @@ CAN_CFG_ERR CAN1_INIT(void)
     CAN1->MCR |= CAN_MCR_INRQ;
     /*Quitting Sleep mode*/	
     CAN1->MCR &= ~CAN_MCR_SLEEP;
-    
-	while(!(CAN1->MSR & CAN_MSR_INAK) && (timeout < CAN_INIT_TIMEOUT_MAX)){
+    while(!(CAN1->MSR & CAN_MSR_INAK) && (timeout < CAN_INIT_TIMEOUT_MAX)){
 		timeout++;	// Waiting for a flag CAN_MSR_INAK (This means, Initialization mode started)
 	};
-	if(!(CAN1->MSR & CAN_MSR_INAK)){ // If a timeout passed away and CAN_MSR_INAK had not come up - Error
+    if(!(CAN1->MSR & CAN_MSR_INAK)){ // If a timeout passed away and CAN_MSR_INAK had not come up - Error
 		return CAN_CFG_FAILED;
 	}
     /* setting the baudrate to oprate at 500kbps*/
     CAN1->BTR &= ~CAN_BTR_TS1; // reset value
-	CAN1->BTR &= ~CAN_BTR_TS2; // reset value
-	CAN1->BTR &= ~CAN_BTR_SJW; // reset value
-	CAN1->BTR |= 0x0003u;       //Prescaler
-	CAN1->BTR |= (0x0A << 16); //BS1
-	CAN1->BTR |= (0x05 << 20); //BS2
+    CAN1->BTR &= ~CAN_BTR_TS2; // reset value
+    CAN1->BTR &= ~CAN_BTR_SJW; // reset value
+    CAN1->BTR |= 0x0003u;       //Prescaler
+    CAN1->BTR |= (0x0A << 16); //BS1
+    CAN1->BTR |= (0x05 << 20); //BS2
 
     /* Enabling reception pending interrupt*/
     CAN1->IER |= CAN_IER_FMPIE0;
@@ -52,29 +51,29 @@ CAN_CFG_ERR CAN1_INIT(void)
     CAN1->MCR &= ~CAN_MCR_INRQ;
     /* Reset timeout*/
     timeout=0;
-	while((CAN1->MSR & CAN_MSR_INAK) && (timeout < CAN_INIT_TIMEOUT_MAX)){
+    while((CAN1->MSR & CAN_MSR_INAK) && (timeout < CAN_INIT_TIMEOUT_MAX)){
 		timeout++;
 	}; // Waiting for both CAN_MSR_SLAK and CAN_MSR_INAK flags down(This means, Normal mode started)
 
-	if((!(CAN1->MSR & CAN_MSR_INAK)) && (!(CAN1->MSR & CAN_MSR_SLAK))){
+    if((!(CAN1->MSR & CAN_MSR_INAK)) && (!(CAN1->MSR & CAN_MSR_SLAK))){
 	//Check SLAK and INAK flags. If they are reset, Controller has entered normal mode
 		return CAN_CFG_FAILED;
 	}
     /* init filters */
-	CAN1->FMR |= CAN_FMR_FINIT; // This bit starts Filter Initialization
-	CAN1->FM1R &= ~0xFFFFFFFF;
-	CAN1->FS1R &= ~0xFFFFFFFF;
-	CAN1->FFA1R &= ~0xFFFFFFFF;
-	CAN1->FA1R	&= ~0xFFFFFFFF;
-	CAN1->sFilterRegister[0].FR1 &= ~0xFFFFFFFF; // CAN->F0R1
-	CAN1->sFilterRegister[0].FR2 &= ~0xFFFFFFFF;
-	CAN1->FM1R  |=  0x00; // 0 - Mask mode
-	CAN1->FS1R  |=  0x01; // 1 - 32-bit
-	CAN1->FFA1R |=  0x00; // 0 - FIFO 0
-	CAN1->FA1R	|=  0x01;	// 1 - Active
-	CAN1->sFilterRegister[0].FR1 |=  0x00; // As long as we accept all the traffic filter value is 0
-	CAN1->sFilterRegister[0].FR2 |=  0x00;
-	CAN1->FMR &= ~CAN_FMR_FINIT; //Filter Initialization completed
+    CAN1->FMR |= CAN_FMR_FINIT; // This bit starts Filter Initialization
+    CAN1->FM1R &= ~0xFFFFFFFF;
+    CAN1->FS1R &= ~0xFFFFFFFF;
+    CAN1->FFA1R &= ~0xFFFFFFFF;
+    CAN1->FA1R	&= ~0xFFFFFFFF;
+    CAN1->sFilterRegister[0].FR1 &= ~0xFFFFFFFF; // CAN->F0R1
+    CAN1->sFilterRegister[0].FR2 &= ~0xFFFFFFFF;
+    CAN1->FM1R  |=  0x00; // 0 - Mask mode
+    CAN1->FS1R  |=  0x01; // 1 - 32-bit
+    CAN1->FFA1R |=  0x00; // 0 - FIFO 0
+    CAN1->FA1R	|=  0x01;	// 1 - Active
+    CAN1->sFilterRegister[0].FR1 |=  0x00; // As long as we accept all the traffic filter value is 0
+    CAN1->sFilterRegister[0].FR2 |=  0x00;
+    CAN1->FMR &= ~CAN_FMR_FINIT; //Filter Initialization completed
     return CAN_CFG_PASSED;
 }
 
