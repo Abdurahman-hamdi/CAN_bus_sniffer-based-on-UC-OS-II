907ec6a062215cc9e221c46aa3937d9d460490f8
Abdurahman-hamdi <abdurrahman.hamdi.95@gmail.com>
2024-08-23 01:15:48 +0300

    prepare for cmake

--- Cmake/GNU-ARM-Toolchain.cmake
+++ Cmake/GNU-ARM-Toolchain.cmake
@@ -14,25 +14,20 @@ set(CMAKE_OBJDUMP arm-none-eabi-objdump)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) 
 
 # core flags
-set(CORE_FLAGS "-mthumb -mcpu=cortex-m4 -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb-interwork --specs=nano.specs --specs=nosys.specs ${ADDITIONAL_CORE_FLAGS}")
+set(CORE_FLAGS "-mthumb -mcpu=cortex-m4 -mlittle-endian -mfpu=fpv4-sp-d16 -mfloat-abi=hard --specs=nano.specs --specs=nosys.specs ${ADDITIONAL_CORE_FLAGS}")
 
 # compiler: language specific flags
-set(CMAKE_C_FLAGS "${CORE_FLAGS} -fno-builtin -Wall -std=gnu11 -fdata-sections -ffunction-sections -DSTM32F4XX -DSTM32F40XX -DUSE_STDPERIPH_DRIVER -g3 -gdwarf-2" CACHE INTERNAL "c compiler flags")
+set(CMAKE_C_FLAGS "${CORE_FLAGS} -fno-builtin -Wall -std=gnu11 -DSTM32F4XX -DSTM32F40XX -DUSE_STDPERIPH_DRIVER -DUSE_STM32F4_DISCOVERY-fdata-sections -ffunction-sections -g3 -gdwarf-2" CACHE INTERNAL "c compiler flags")
 set(CMAKE_C_FLAGS_DEBUG "" CACHE INTERNAL "c compiler flags: Debug")
 set(CMAKE_C_FLAGS_RELEASE "" CACHE INTERNAL "c compiler flags: Release")
 
-set(CMAKE_CXX_FLAGS "${CORE_FLAGS} -fno-rtti -fno-exceptions -fno-builtin -Wall --warn-no-char-subscripts -std=gnu++11 -fdata-sections -ffunction-sections -g -ggdb3" CACHE INTERNAL "cxx compiler flags")
-set(CMAKE_CXX_FLAGS_DEBUG "" CACHE INTERNAL "cxx compiler flags: Debug")
-set(CMAKE_CXX_FLAGS_RELEASE "" CACHE INTERNAL "cxx compiler flags: Release")
-
-set(CMAKE_CXX_FLAGS "${CORE_FLAGS} -fno-rtti -fno-exceptions -fno-builtin -Wall -std=gnu++11 -fdata-sections -ffunction-sections -g -ggdb3" CACHE INTERNAL "cxx compiler flags")
+set(CMAKE_CXX_FLAGS "${CORE_FLAGS} -fno-rtti -fno-exceptions -fno-builtin -Wall -std=gnu++11 -DSTM32F4XX -DSTM32F40XX -DUSE_STDPERIPH_DRIVER -DUSE_STM32F4_DISCOVERY -fdata-sections -ffunction-sections -g -ggdb3" CACHE INTERNAL "cxx compiler flags")
 set(CMAKE_CXX_FLAGS_DEBUG "" CACHE INTERNAL "cxx compiler flags: Debug")
 set(CMAKE_CXX_FLAGS_RELEASE "" CACHE INTERNAL "cxx compiler flags: Release")
 
 set(CMAKE_ASM_FLAGS "${CORE_FLAGS} -g -ggdb3 -D__USES_CXX" CACHE INTERNAL "asm compiler flags")
 set(CMAKE_ASM_FLAGS_DEBUG "" CACHE INTERNAL "asm compiler flags: Debug")
 set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "asm compiler flags: Release")
-
 
 # search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -47,3 +42,4 @@ find_program(ARM_SIZE_EXECUTABLE arm-none-eabi-size)
 find_program(ARM_GDB_EXECUTABLE arm-none-eabi-gdb)
 find_program(ARM_OBJCOPY_EXECUTABLE arm-none-eabi-objcopy)
 find_program(ARM_OBJDUMP_EXECUTABLE arm-none-eabi-objdump)
+
